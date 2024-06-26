02f94fefd9e469762460290f7fd766d9526a464b
JJ Merelo <jjmerelo@gmail.com>
2024-06-26 10:33:34 +0200

    Añadiendo datos baseline

--- code/scripts/run-pumas.sh
+++ code/scripts/run-pumas.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env sh
 
-COMMANDS=$@
-OUTPUT=baseline.json
 
-for i in $(seq 15)
+FILENAME=$1
+
+for i in 512 1024 2048
 do
-    pumas run -i100 --json > $OUTPUT & $COMMANDS
+    echo "Size ${i}"
+    OUTPUT="${FILENAME}-${i}.json"
+    echo "Saving to ${OUTPUT}"
+    pumas run -i 100 --json > $OUTPUT &
+    for j in $(seq 15)|
+    do
+        C++/chromosomes $i
+    done
     kill $!
 done
