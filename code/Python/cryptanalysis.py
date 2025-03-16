a1ab5905da9d9c12267ad085f1a4b82f1b94d3fe
JJ Merelo <jjmerelo@gmail.com>
2025-03-16 11:38:26 +0100

    #12 Make exe

---
+++ code/Python/cryptanalysis.py
@@ -0,0 +1,31 @@
+!#/usr/bin/env python
+
+import string
+from collections import Counter
+
+def read_file(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        return file.read()
+
+def eliminate_whitespace_and_punctuation(text):
+    translator = str.maketrans('', '', string.whitespace + string.punctuation)
+    return text.translate(translator)
+
+def calculate_frequency(text):
+    return Counter(text)
+
+def print_frequency_markdown(frequency):
+    print("| Symbol | Frequency |")
+    print("|--------|-----------|")
+    for symbol, freq in frequency.items():
+        print(f"|   {symbol}   |    {freq}    |")
+
+def main():
+    file_path = '../data/malavoglia.txt'
+    text = read_file(file_path)
+    cleaned_text = eliminate_whitespace_and_punctuation(text)
+    frequency = calculate_frequency(cleaned_text)
+    print_frequency_markdown(frequency)
+
+if __name__ == "__main__":
+    main()
