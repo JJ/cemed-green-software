862588e7e3bb9b61e1d557a19fab95a79fe1165c
JJ Merelo <jjmerelo@gmail.com>
2025-03-16 11:54:23 +0100

    #12 converting

--- code/Python/cryptanalysis.py
+++ code/Python/cryptanalysis.py
@@ -2,6 +2,8 @@
 
 import string
 from collections import Counter
+import sys
+import unicodedata
 
 def read_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
@@ -9,7 +11,11 @@ def read_file(file_path):
 
 def eliminate_whitespace_and_punctuation(text):
     translator = str.maketrans('', '', string.whitespace + string.punctuation + '«»“”‘’')
-    return ''.join(filter(str.isalpha, text.translate(translator)))
+    text = ''.join(filter(str.isalpha, text.translate(translator)))
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', text)
+        if unicodedata.category(c) != 'Mn'
+    )
 
 def calculate_frequency(text):
     return Counter(text.lower())
@@ -21,7 +27,11 @@ def print_frequency_markdown(frequency):
         print(f"|   {symbol}   |    {freq}    |")
 
 def main():
-    file_path = 'data/malavoglia.txt'
+    if len(sys.argv) != 2:
+        print("Usage: python cryptanalysis.py <file_path>")
+        sys.exit(1)
+
+    file_path = sys.argv[1]
     text = read_file(file_path)
     cleaned_text = eliminate_whitespace_and_punctuation(text)
     frequency = calculate_frequency(cleaned_text)
