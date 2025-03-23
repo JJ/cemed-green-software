c2c0e104dad67f63ceb02ec2e907016d8f58bbdb
JJ Merelo <jjmerelo@gmail.com>
2025-03-23 09:57:27 +0100

    :bug:

--- code/scripts/plot-desktop-amd-baseline.R
+++ code/scripts/plot-desktop-amd-baseline.R
@@ -2,4 +2,4 @@ library(ggplot2)
 
 amd_desktop_base <- read.csv("../data/test-23-Mar-09-04-38.csv")
 
-ggplot(amd_desktop_base, aes(x=PKG,y=seconds))+geom_point()+geom_smooth(method="lm")+theme_minimal()+labs(title="Desktop AMD Baseline",x="PKG (Watts)",y="Time (seconds)")
+ggplot(amd_desktop_base, aes(x=PKG,y=seconds))+geom_point()+geom_smooth(method="lm")+theme_minimal()+labs(title="Desktop AMD Baseline",x="PKG (Joules)",y="Time (seconds)")
