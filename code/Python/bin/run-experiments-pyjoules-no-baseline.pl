05843db8ef2ae99ca12774170a19a20ed77c384b
JJ Merelo <jjmerelo@gmail.com>
2025-03-23 10:38:23 +0100

    #20 experiments run with baseline

---
+++ code/Python/bin/run-experiments-pyjoules-no-baseline.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use v5.14;
+
+my $preffix = shift || die "I need a preffix for output files";
+
+my $data_dir = shift || "../data";
+my $platform = shift || "desktop-AMD";
+my $ITERATIONS = 30;
+my ($mon,$day,$hh,$mm,$ss) = localtime() =~ /(\w+)\s+(\d+)\s+(\d+)\:(\d+)\:(\d+)/;
+my $suffix = "$day-$mon-$hh-$mm-$ss";
+
+open my $fh, ">", "$data_dir/$preffix-$suffix.csv";
+
+say $fh "Platform,File,PKG,seconds";
+
+for my $f ( qw( manzoni_i_promessi_sposi_1840 malavoglia 3romanzi ) ) {
+  my @results;
+  for ( my $i = 0; $i < $ITERATIONS; $i++ ) {
+    my $command = "./.venv/bin/python bin/sacrypt-baseline.py ../data/$f.txt";
+    my $output = `$command 2>&1`;
+    say $output;
+    my ($duration, $PKG) = ( $output =~ /process_file.+duration : (\S+); package_0 : (\S+);/ );
+    say "$duration $PKG";
+    say $fh "$platform; $f; ",$PKG/1e6,"; $duration";
+  }
+}
+close $fh;
