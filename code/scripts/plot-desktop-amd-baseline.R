527a6d9b6943be4b7e36ed6b977677638eb1e5f1
JJ Merelo <jjmerelo@gmail.com>
2025-03-24 19:13:32 +0100

    #19 graphing expenses per book

--- code/scripts/plot-desktop-amd-baseline.R
+++ code/scripts/plot-desktop-amd-baseline.R
@@ -2,4 +2,10 @@ library(ggplot2)
 
 amd_desktop_base <- read.csv("../data/test-23-Mar-09-04-38.csv")
 
-ggplot(amd_desktop_base, aes(x=PKG,y=seconds,shape=File))+geom_point()+geom_smooth(method="lm")+theme_minimal()+labs(title="Desktop AMD Baseline",x="PKG (Joules)",y="Time (seconds)")
+ggplot(amd_desktop_base, aes(x=PKG,y=seconds,shape=File))+geom_point()+geom_smooth(method="lm")+labs(title="Desktop AMD Baseline",x="PKG (Joules)",y="Time (seconds)")
+ggsave("../../img/time-vs-pkg-amd-libri.png",width=9, height=5)
+
+amd_desktop_base$mb_per_J <- amd_desktop_base$size/ (1024*1024 * amd_desktop_base$PKG )
+
+ggplot(amd_desktop_base, aes(x=File,y=mb_per_J))+geom_boxplot(notch=T)+labs(title="Desktop AMD Baseline",x="File",y="MB per Joule")
+ggsave("../../img/mb-per-joule-amd-libri.png",width=9, height=5)
