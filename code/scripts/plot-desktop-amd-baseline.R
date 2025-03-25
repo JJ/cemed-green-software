922d767a487b50cb6582c10b3821b293924f0708
JJ Merelo <jjmerelo@gmail.com>
2025-03-25 17:16:30 +0100

    Plotting results

--- code/scripts/plot-desktop-amd-baseline.R
+++ code/scripts/plot-desktop-amd-baseline.R
@@ -11,3 +11,8 @@ ggplot(amd_desktop_base, aes(x=File,y=mb_per_J))+geom_boxplot(notch=T)+labs(titl
 ggsave("../../img/mb-per-joule-amd-libri.png",width=9, height=5)
 
 amd_desktop_base$delta_PKG <- amd_desktop_base$PKG - 21.3536*amd_desktop_base$seconds
+ggplot(amd_desktop_base, aes(x=delta_PKG,y=seconds,shape=File))+geom_point()+geom_smooth(method="lm")+labs(title="Desktop AMD Energy",x="Delta PKG (Joules)",y="Time (seconds)")
+
+ggplot(amd_desktop_base, aes(y=delta_PKG,x=File))+geom_violin()+labs(title="Energy per file",x="File",y="PKG -Joules")
+
+ggsave("../../img/boxplot-delta-pkg-amd-libri.png",width=9, height=5)
