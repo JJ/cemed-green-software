c37da99d035ad05ebebfb5a8569e976d093ebcf1
JJ Merelo <jjmerelo@gmail.com>
2025-03-25 20:44:27 +0100

    #30 using only standalone scripts

--- code/Python/bin/sacrypt-mac.py
+++ code/Python/bin/sacrypt-mac.py
@@ -4,7 +4,7 @@ import sys
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
-from sacrypt import process_text_v0, process_text_v1
+from sacrypt import process_text_v0, process_text_v1, print_frequency_markdown
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -14,9 +14,11 @@ if __name__ == "__main__":
     version = sys.argv[2] if len(sys.argv) > 2 else "v0"
 
     if version == "v0":
-        process_text_v0( file_path)
+        frequencies = process_text_v0( file_path)
     elif version == "v1":
-        process_text_v1( file_path)
+        frequencies = process_text_v1( file_path)
     else:
         print("Invalid version. Use 'v0' or 'v1'.")
         sys.exit(1)
+
+    print_frequency_markdown(frequencies)
