8231b7bc4d3d7e76578a14368fdd4199e53a4fb2
JJ Merelo <jjmerelo@gmail.com>
2025-03-25 20:50:27 +0100

    #30 running on the Mac

--- code/Python/bin/run-pumas.sh
+++ code/Python/bin/run-pumas.sh
@@ -4,15 +4,15 @@
 
 FILENAME=$1
 
-for i in 512 1024 2048
+for i in 3romanzi malavoglia manzoni_i_promessi_sposi_1840
 do
-    echo "Size ${i}"
+    echo "File ${i}"
     OUTPUT="${FILENAME}-${i}.json"
     echo "Saving to ${OUTPUT}"
     pumas run -i 100 --json > $OUTPUT &
-    for j in $(seq 15)|
+    for j in $(seq 30)
     do
-        C++/chromosomes $i
+        bin/sacrypt-mac.py ../data/$i.txt
     done
     kill $!
 done
